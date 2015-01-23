60ce015ec6561654cfe6ee5e5c7d7011d1c2d342
kevintomlee <kevintomlee@hotmail.com>
2015-01-24 00:13:57 +0800

    added priority functions

--- puzzle/puzzle.py
+++ puzzle/puzzle.py
@@ -1,22 +1,46 @@
+import random
 
-class Puzzle:
-	''' N by N puzzle
+class Board:
+	''' N by N slider puzzle
 	'''
 	Direction = ('UP', 
 				 'DOWN',
 				 'LEFT',
 				 'RIGHT')
 
-	def __init__(self, n):
-		self.block = []
-		self.N = n
-		self.freeTile = n*n-1
-		for i in range(n*n-1):
-			self.block.append(i+1)
-		self.block.append(0)
+	def __init__(self, n=3, tiles=None):
+		if tiles!=None:
+			self.block = []
+			for i in range(len(tiles)):
+				if tiles[i] == 0:
+					self.freeTile = i
+				self.block.append(tiles[i])
+			import math
+			self.N = int(math.sqrt(len(tiles)))
+		else:
+			self.N = n
+			self.freeTile = n*n -1
+			self.block, self.freeTile = self.genRanSeq()
 
-	def genRanSeq(self):
-		pass
+	def genRanSeq(self, s=1):
+		sequence = []
+		newseq = []
+		length = self.N*self.N
+		for i in range(length):
+			sequence.append(i)
+			newseq.append(0)
+		seqrange = length
+		random.seed(s)
+		freetileidx = 0
+		for i in range(length):
+			e = random.randrange(seqrange)
+			newseq[i] = sequence[e]
+			if newseq[i]==0:
+				freetileidx = i
+			sequence[seqrange-1], sequence[e] = sequence[e], sequence[seqrange-1]
+			seqrange -= 1
+		return newseq, freetileidx
+
 
 	def getXY(self, index):
 		x = index // self.N
@@ -25,6 +49,41 @@ class Puzzle:
 
 	def getIndex(self, x, y):
 		return x*self.N + y
+
+	def hamming(self):
+		res = 0
+		for i in range(len(self.block)-1):
+			if i+1 != self.block[i]:
+				res += 1
+		return res
+
+	def manhattan(self):
+		res = 0
+		for i in range(len(self.block)):
+			if self.block[i]!=0:
+				x1, y1 = self.getXY(i)
+				x2, y2 = self.getXY(self.block[i]-1)
+				dif = abs(x1-x2) + abs(y1-y2)
+				res += dif
+		return res
+
+	def __eq__(self, other):
+		if isinstance(other, Board):
+			if len(self.block) == len(other.block):
+				for i in range(len(self.block)):
+					if self.block[i]!=other.block[i]:
+						return False
+				return True
+		return False
+
+	def neighbors(self):
+		n = []
+		for d in self.Direction:
+			b = Board(tiles=self.block)
+			b.move(d)
+			if self != b:
+				n.append(b)
+		return n
 
 	def move(self, direction):
 		x, y = self.getXY(self.freeTile)
@@ -70,18 +129,19 @@ class Puzzle:
 
 if __name__ == '__main__' :
 	N = 3
-	grid = Puzzle(N)
+	board = Board(N)
+	print(board)
 	a = input('move:')
 	while a!='q':
 		if a == 'q':
 			break
 		if a == 'u':
-			grid.move('DOWN')
+			board.move('DOWN')
 		if a == 'd':
-			grid.move('UP')
+			board.move('UP')
 		if a == 'r':
-			grid.move('LEFT')
+			board.move('LEFT')
 		if a == 'l':
-			grid.move('RIGHT')
-		print(grid)
+			board.move('RIGHT')
+		print(board)
 		a = input('move:')
